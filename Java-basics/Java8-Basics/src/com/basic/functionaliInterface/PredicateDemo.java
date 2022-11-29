53aad1ad6b6817fbab6316649ec428d4524d6a8c
unknown <cogjava4575@iiht.tech>
2022-11-29 14:58:51 +0530

    updating code

--- Java-basics/Java8-Basics/src/com/basic/functionaliInterface/PredicateDemo.java
+++ Java-basics/Java8-Basics/src/com/basic/functionaliInterface/PredicateDemo.java
@@ -1,10 +1,21 @@
 package com.basic.functionaliInterface;
 
-public class PredicateDemo {
+import java.util.function.Predicate;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class PredicateDemo implements Predicate<Integer> {
 
+	@Override
+	public boolean test(Integer t) {
+		if(t % 2 == 0) {
+			return true;
+		}
+		return false;
 	}
+	
+	public static void main(String[] args) {
+		Predicate<Integer> predicate=new PredicateDemo();
+		System.out.println(predicate.test(4));
+	}
+
 
 }
