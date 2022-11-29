c502db47bdc92ceeb0f8ad62143676d0663a1cb4
unknown <cogjava4575@iiht.tech>
2022-11-29 15:19:00 +0530

    updating

--- Java-basics/Java8-Basics/src/com/basic/functionaliInterface/PredicateDemo.java
+++ Java-basics/Java8-Basics/src/com/basic/functionaliInterface/PredicateDemo.java
@@ -1,20 +1,32 @@
 package com.basic.functionaliInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer> {
+public class PredicateDemo {
 
-	@Override
-	public boolean test(Integer t) {
-		if(t % 2 == 0) {
-			return true;
-		}
-		return false;
-	}
+	
+	
+/*	public static void main(String[] args) {
+		Predicate<Integer> predicate=(t)-> {
+			if(t % 2 == 0) {
+				return true;
+			}
+			return false;
+		};
+		System.out.println(predicate.test(55));*/
 	
 	public static void main(String[] args) {
-		Predicate<Integer> predicate=new PredicateDemo();
-		System.out.println(predicate.test(4));
+		
+		//Predicate<Integer> predicate=(t)->t % 2 == 0;
+		//System.out.println(predicate.test(50));
+		
+		//List<Integer> integerList = Arrays.asList(1,2,3,4,5);
+		//integerList.stream().filter(predicate).forEach(number-> System.out.println("Printing :"+ number));
+		
+		List<Integer> integerList = Arrays.asList(1,2,3,4,5);
+		integerList.stream().filter((t)->t % 2 == 0).forEach(number-> System.out.println("Printing :"+ number));
 	}
 
 
