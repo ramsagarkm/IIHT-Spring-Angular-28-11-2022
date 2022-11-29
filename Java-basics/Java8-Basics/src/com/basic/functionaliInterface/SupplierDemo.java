01e9ac4e876fdd845bf347c863dbc4100a8b4e03
unknown <cogjava4575@iiht.tech>
2022-11-29 15:28:18 +0530

    supplier demo code

---
+++ Java-basics/Java8-Basics/src/com/basic/functionaliInterface/SupplierDemo.java
@@ -0,0 +1,18 @@
+package com.basic.functionaliInterface;
+
+import java.util.function.Supplier;
+
+public class SupplierDemo implements Supplier<String>{
+	
+	@Override
+	public String get() {
+		// TODO Auto-generated method stub
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+
+		Supplier<String> supplier=new SupplierDemo();
+		System.out.println(supplier.get());
+	}
+}
