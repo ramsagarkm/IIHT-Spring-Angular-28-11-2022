b1e55c9d00c721cf9b5e20b969167fcfa70d4487
unknown <cogjava4575@iiht.tech>
2022-11-29 14:41:52 +0530

    updated code

--- Java-basics/Java8-Basics/src/com/basic/functionaliInterface/ConsumerDemo.java
+++ Java-basics/Java8-Basics/src/com/basic/functionaliInterface/ConsumerDemo.java
@@ -1,15 +1,17 @@
 package com.basic.functionaliInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
 
 	public static void main(String[] args) {
 		
-		Consumer<Integer> consumer= (t) ->
-			System.out.println("Printing : "+ t);
+		Consumer<Integer> consumer= (t) -> System.out.println("Printing : "+ t);
 			consumer.accept(4000);
-			
+			List<Integer> integerList = Arrays.asList(1,2,3,4,5);
+			integerList.stream().forEach(consumer);
 		}
 
 	}
