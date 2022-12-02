701563764cc7648db9db292b3154455def64def8
unknown <cogjava4575@iiht.tech>
2022-12-02 13:32:44 +0530

    Updating code

--- HTML,JS,TS_basics/index.js
+++ HTML,JS,TS_basics/index.js
@@ -1,170 +1,177 @@
-       /* var name="Ramsagar";
-        var age=32;
-        var isGraduate=true;
-        var laptop;
-        laptop="LG";
-        console.log(name);
+var name = "Nishant"; // because this is isn't insid eany function / opertaion this is a global variable
+// var age = 32; // define a variable
+// var isGraduate = true;
+// var laptop;
+// laptop = "lg";
+// console.log(name);
 
-        function alertUser(){
-            var favChips="Lays Blue"//this variable is defined and destroy
-            console.log("This user has loged in "+name);
-            alert("This user has loged in "+name);
-        }
-        alertUser();
-        console.log("This user has loged in "+name);
-
-        //if, else, else if statement
-        function operators(num){
-            if(num>20){
-                console.log("Number is greater then 20");
-            }else if(num==20){
-                console.log("Number is equals then 20");
-            }else{
-                console.log("Number is smaller then 20");
-            }
-        }
-
-        operators(87); */
-//Switch
-  /*      function creditScore(score){
-            var creditLimit;
-            switch(score){
-                case 800 : creditLimit="Above 1 Lac";
-                break;
-                case 700 : creditLimit="50 K to 1 Lac";
-                break;
-                case 600 : creditLimit="10 K to 25K";
-                break;
-                default : creditLimit=" Not eligible";
-            }
-            console.log("Credit limit"+creditLimit );
-        }
-        creditScore(700);
-*/
-     var car=new Object();
-     car.brand="Marcdes";
-     car.model="c class";
-     car.price=6000000
-     console.log(car);
-
-     var names= ["Nishat","Ramsagar","Mukesh"];
-     console.log(names);
-
-     //Class creating in below ways
-     //class Car{
-     let  Car= class{
-
-        constructor(brand, modelName, price){
-            this.brand=brand;
-            this.modelName=modelName;
-            this.price=price;
-        }
-        print(){
-            console.log("This is a : "+ this.brand+ " "+this.modelName +" Which cost : "+ this.price);
-        }
-     }
-     var C1= new Car("Mercedes","A Class", 4000000);
-     var C2= new Car("BMW","C Class", 8000000);
-     C1.print();
-     C2.print();
-
-     class TodaysDate extends Date{ //example to make a class a child class of inbuilt js class.
-        constructor(){
-            super();
-        }
-
-        printDate(){
-            console.log(this.getDate());
-        }
-     }
-     let today=new TodaysDate();
-     today.printDate();
-
-
-     class AppleLaptops{ // Parent class
-         constructor(brands){
-             this.brands=brands;
-
-         }
-     }
-
-     class MacBookPro extends AppleLaptops{ // child class
-        constructor(model,processor,ram){
-            super();
-            this.model=model;
-            this.processor=processor;
-            this.ram=ram;
-        }
-
-     }
-     let l1=new MacBookPro("2021 15 MacBookPro","17",16)
-     console.log(l1.brands + " "+ l1.model + " "+ l1.processor+  " "+ l1.ram);
-
-     //Arrays joining below
-     let fruits=["Apple","Banana","Primple"]
-     let secondFruits=["Mango","Lichi"]
-     let AllFruits=[...fruits, ...secondFruits]; // ... is spread operator and it copies all the elements of the array named after the use ...
-     console.log(AllFruits);
-
-
-// //loop
-// function counting(){
-//     for(let i=1;i<10;i++){
-//         console.log(i);
-//     }
+// function alertUser() {
+//   var favChips = "Lays Blue"; // this variable is defined and destroyed inside the function itself so it cant be accessed outside this function
+//   console.log("This user has logged in" + name);
+//   //  alert("This user has logged in" + name);
 // }
-// counting();
+// alertUser();
+// // if. else, else if statements
+// function operators(num) {
+//   if (num > 20) {
+//     console.log("Number is greater than 20");
+//   } else if (num == 20) {
+//     console.log("number is equal to 20");
+//   } else {
+//     console.log("number is smaller than 20 ");
+//   }
+// }
+// operators(87);
+function creditScore(score) {
+  var creditLimit;
+  switch (score) {
+    case 800:
+      creditLimit = "above 1 lac";
+      break;
+    case 700:
+      creditLimit = "50,000 to 1,00,000";
+      break;
+    case 600:
+      creditLimit = "10,000 tp 25,000";
+      break;
+    default:
+      creditLimit = "not eligible";
+  }
+  console.log(creditLimit);
+}
 
 
-let counting = () =>{ // this is how you define on inline / arrow  function
-    for(let i=1;i<10;i++){
-   console.log(i);
+
+var car= new Object();
+car.brand = "Mercedes"
+car.model="C class"
+car.price = 6000000
+
+console.log(car);
+
+var names = ["nishant", "vivek", "Abhishek"];
+console.log(names);
+
+creditScore(800);
+
+
+let Car = class{
+    // inialize any object 
+
+    constructor(brand, modelName, price) {
+        this.brand = brand;
+        this.modelName = modelName;
+        this.price = price;
+    }
+    print() {
+        console.log("this is a :" + this.brand+ " "+ this.modelName + " which costs : " + this.price);
+    }
+}
+
+var C1 = new Car("Mercedes", "A class" , 4000000)
+var C2 = new Car("BMW", "3-series", 5000000);
+C1.print();
+C2.print();
+
+
+
+class TodaysDate extends Date { // example to make a class a child class of a inbuild JS class
+  constructor() {
+    super();
+  }
+
+  printDate() {
+    console.log(this.getDate());
+  }
+}
+let today = new TodaysDate();
+today.printDate();
+
+
+class AppleLaptops { // parent class
+  constructor(brand) {
+    this.brand = "Apple";
+
+  }
+}
+class MacBookPro extends AppleLaptops { // child class
+  constructor(model,processor,ram) {
+    super();
+    this.model =model;
+    this.processor = processor;
+    this.ram = ram
+  }
+}
+
+let l1 = new MacBookPro("2021 15` Macbook pro", "i7", 16 )
+console.log(l1.brand + " " +l1.model + " " + l1.processor + " " + l1.ram);
+
+
+let fruits = ["Apple", "Banana", "Pineapple"]
+
+let seasonalFruits = ["Mango", "Lichi"]
+
+let allFruits =[ ...fruits, ...seasonalFruits]; // ... is the spread operator and it copies all the elements of the array named after the use of ...
+
+console.log(allFruits);
+
+// an concept of arrow function was introduced
+
+ // conventional method 
+
+let counting = () => {  // this is how you define an inline / arrow function 
+   for (let i = 1; i < 10; i++) {
+  console.log(i);
 }}
+
 counting();
 
-//There are three keyword that can be used to declare a variable in javascript.
-//Var - this is a scop of function, it can be re-declear
-var num1= 1000;
-var num1= 2000;
+// there ar three keywords that can be used to declare a variable in javascript 
+// var -> this has a scope of a function , it can be re-declared
+
+var num1 = 1000;
+var num1 = 2000;
 console.log(num1);
 
-//let is a keyword  -> was announce with es6, it has a block scop, a variable decleared with let cannot be re-defineed
-let a =100;
-function addNumber(){
-    a=130;
-    let b=100;
-    console.log(a+b);
+// let keyword -> was announced with es6 , it has a block scope  , a variable declared using let cannot be re-defined 
+let a = 100;
+function addNumbers() {
+  a = 230;
+  let b = 100
+  console.log(a+b);
 }
-addNumber();
-console.log(a+ 1000);
+ addNumbers();
+ console.log(a + 1000);
 
-//const -> it stands for constent so you can not change the value directly , this also have scop of block.
+ // const -> it stands for contant so you cannot change the value diretly , this also has a scope of block 
+ function addNumbers1() {
+  const num2 = 230;
+  // const num2 = 900;
 
-function addNumbers1(){
-    const num2 =230;
-    //const num2 =900;
-
-}
-
-
-function Dog(){
-    this.dogName="Lab"
-    throw new error("This is not wild dog");
-}
-dogs.prototype.display = function(){
-    return "This dog breed  is : "+ this.dogName;  
+  console.log(num2);
 }
 
-//constructor  function
-function Animal (dogName){
-    this.dogName=dogName;
+addNumbers1();
+
+
+function dogs() {
+  this.dogname = "Lab"
+  throw new error("This is not a wild dog")
 }
 
-//Create object without using constructor
-Animal.prototype=Object.create(dogs.prototype)
-let tomy=new Animal("German shephred")
-console.log(tomy.display)
+dogs.prototype.display= function(){
+  return "this dogs breed is :" + this.dogname;
+}
 
-function Greet(){
-    alert("Greetings fellow developers")
+// constructor function 
+function Animal(dogname) {
+  this.dogname = dogname
+}
+// create an object without using the constructor 
+Animal.prototype= Object.create(dogs.prototype)
+let tommy = new Animal("German shepherd")
+console.log(tommy.display());
+
+function Greet() {
+  alert("Greetings fellow Developer")
 }
--- HTML,JS,TS_basics/promise.js
+++ HTML,JS,TS_basics/promise.js
@@ -1,16 +1,30 @@
+//
 
-const fun1=() =>{
-    console.log("This is fun1")
-}
-const fun2=() =>{
-    setTimeout( ()=>{
-    console.log("This is fun2 expected to time out in");
-},5000);
-console.log("This is fun2")
-}
-const fun3=() =>{
-    console.log("This is fun3")
-}
-fun1();
-fun2();
-fun3();
+const fun1 = () => {
+    return "This is function 1";
+};
+// const fun2 =  () => {
+//     setTimeout( () => {
+//        return "This is function 2"
+//     } ,  5000);
+// }
+const fun2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("This is function 2");
+        }, 2000);
+    });
+};
+
+const fun3 = () => {
+    return "this is function 3 ";
+};
+
+const callFun = async () => {
+    let val1 = fun1();
+    console.log(val1);
+    let val2 = await fun2();
+    console.log(val2);
+    let val3 = fun3();
+    console.log(val3);
+};
