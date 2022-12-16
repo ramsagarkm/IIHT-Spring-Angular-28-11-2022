81b591d06e03f0c67300f9c5ad9fbf3896976e34
unknown <cogjava4575@iiht.tech>
2022-12-16 16:11:51 +0530

    delete row

--- Client-side/helloworld/src/app/Services/user.service.ts
+++ Client-side/helloworld/src/app/Services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { getMultipleValuesInSingleSelectionError } from '@angular/cdk/collections';
 
 const BASE_URL = 'http://localhost:5000/users';
 
@@ -16,6 +17,11 @@ export class UserService {
   }) {
     return this.http.post(BASE_URL, user);
   }
-
+  getUsers(){
+    return this.http.get(BASE_URL);
+  }
+  deleteUsers(user:any){
+    return this.http.delete(BASE_URL +"/"+user.id);
+  }
   constructor(private http: HttpClient) {}
 }
--- Client-side/helloworld/src/app/app-routing.module.ts
+++ Client-side/helloworld/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { UserformComponent } from './Components/userform/userform.component';
+import { RegUsersComponent } from './Components/reg-users/reg-users.component';
 
-const routes: Routes = [{path:"", component:HomeComponent}, {path:"register" , component:UserformComponent}];
+const routes: Routes = [{ path: "", component: HomeComponent }, { path: "register", component: UserformComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
